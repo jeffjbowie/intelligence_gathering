f14e555dc5eec3da471dd1334053171603d20362
Jeff Bowie <jeffjbowie@gmail.com>
2020-09-19 08:58:23 -0500

    fixes.

--- csv_example.py
+++ csv_example.py
@@ -1,21 +1,39 @@
-import csv,requests
+import csv
+import requests
 from bs4 import BeautifulSoup
+import pdb
 
+# List of URLS
 urls = [
-    'aliasinfosec.com',
-    'github.com',
-    'news.ycombinator.com'
+	'aliasinfosec.com',
+	'github.com',
+	'news.ycombinator.com'
 ]
 
+results = []
+
+# Loop through URLs
 for u in urls:
-    _r = requests.get(f"https://{u}")
 
-    if _r.status_code == 200:
+	_r = requests.get(f"https://{u}")
 
-        soup = BeautifulSoup(_r.text, 'lxml')
-        title = soup.find('title')
+	# Make sure 200 OK
+	if _r.status_code == 200:
 
-        with open(f"{u}.csv") as f:
-            writer = csv.writer(f)
-            writer.writerow(["Title", "URL"])
-            writer.writerow([title.text, _r.url])
+		soup = BeautifulSoup(_r.text, 'html.parser')
+		title = soup.find('title')
+
+		results.append([title.text, u])
+
+# Open a CSV file, with write attribute.
+with open(f"output.csv", "w") as f:
+
+	# Create a writer, write header.
+	writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+	writer.writerow(["Title", "URL"])
+
+	# Loop through results and write rows.
+	for r in results:
+		writer.writerow([r[0], r[1]])
+
+
